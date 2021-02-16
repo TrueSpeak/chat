b5b1984c9d4abed76fde77ae39248c80e2772e39
truesspeak <kola-kola-321>
2021-02-16 18:35:32 +0300

    update validations, add enumerize

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  extend Enumerize
+
+  ROLES = %i[admin, moderator, default]
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -7,5 +10,8 @@ class User < ApplicationRecord
   has_one :user_black_list
   belongs_to :black_list
 
-  validates :email
+  validates :role, presence: true
+  validates :email, presence: true
+
+  enumerize :role, in: ROLES, predicates: true
 end
